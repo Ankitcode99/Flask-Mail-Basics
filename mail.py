b6aa0c3f362611a488062ecad3d6e77dbcd5f6a3
AnkitCode99 <9999385728ankit@gmail.com>
2020-09-29 01:55:12 +0530

    First Commit for Flask Mail

---
+++ mail.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_mail import Mail, Message
+import os
+
+app = Flask(__name__)
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASS']
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        msg = Message(subject="Realisation",
+                      sender=app.config.get("MAIL_USERNAME"),
+                      recipients=["ankitpandeycu@gmail.com"], # use your email for testing
+                      body="This is a test email I sent with Gmail and Python! But bhai pyaar toh aaj bhi hai dil mei kahi na kahi uske liye...")
+        mail.send(msg)
